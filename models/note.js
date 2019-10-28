70aba7cae516938e73f68ebc45c0e9201aebda28
Peter Eck <jpeckiii@gmail.com>
2019-10-28 13:08:46 -0500

    Remove mongo.js and update note.js

--- models/note.js
+++ models/note.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const url = process.env.MONGODB_URI;
 
-console.log(`Connecting to ${url}`);
+console.log(`Connecting to ${ url }`);
 
 mongoose.connect(url, { useNewUrlParser: true })
     .then(res => {
         console.log(`Connected to MongoDB`);
     })
     .catch(error => {
-        console.log(`Error connecting to MongoDB: ${error}`)
+        console.log(`Error connecting to MongoDB: ${ error.message }`)
     })
 
 const noteSchema = new mongoose.Schema({
@@ -22,7 +22,7 @@ noteSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id,
-        delete returnedObject._v
+            delete returnedObject._v
     }
 });
 
