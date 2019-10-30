f07c212a3fdece503015b56e9068bfdc26026d0c
Peter Eck <jpeckiii@gmail.com>
2019-10-30 13:11:05 -0500

    Move URI to different file

--- models/note.js
+++ models/note.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const url = require('../secret');
 
-const url = process.env.MONGODB_URI.toString();
+const url = url;
 
 console.log(`Connecting to ${ url }`);
 
