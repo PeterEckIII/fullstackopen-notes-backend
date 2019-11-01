d7b97933932622c2f7d0fa0499a67d53475943a7
Peter Eck <jpeckiii@gmail.com>
2019-11-01 14:47:23 -0500

    Add build dir and fix root route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
