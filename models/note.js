8e565b4bfd3dfbceb13c6699ea39fdf80af67bcc
Peter Eck <jpeckiii@gmail.com>
2019-11-01 17:31:04 -0500

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(String(process.env.MONGO_URI), { useNewUrlParser: true })
     .then(result => {
         console.log('Connected to MongoDB')
     })
