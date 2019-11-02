f0a534db4b0ed5a74185e211ea138addb695e7ee
Peter Eck <jpeckiii@gmail.com>
2019-11-02 16:01:07 -0500

    Remove db hyrdation data

--- index.js
+++ index.js
@@ -10,28 +10,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 
-
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         date: "2019-05-30T17:30:31.098Z",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only Javascript",
-//         date: "2019-05-30T18:39:34.091Z",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         date: "2019-05-30T19:20:14.298Z",
-//         important: true
-//     }
-// ]
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + './build/index.html');
     // Issue is with this line - try to get the html file served for this route
