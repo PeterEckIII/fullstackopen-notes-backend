9968898878e27ddcc6014e6439c736a23b99302b
Peter Eck <jpeckiii@gmail.com>
2019-11-06 10:39:02 -0600

    Write first API test

--- utils/config.js
+++ utils/config.js
@@ -3,10 +3,6 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let MONGO_URI = process.env.MONGO_URI
 
-if (process.env.NODE_ENV === 'test') {
-    MONGODB_URI = process.env.TEST_MONGO_URI
-}
-
 module.exports = {
     MONGO_URI,
     PORT
