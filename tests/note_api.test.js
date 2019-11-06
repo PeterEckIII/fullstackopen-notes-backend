2ab5861da02bc15e45a3c8aab73de421fc98b770
Peter Eck <jpeckiii@gmail.com>
2019-11-06 10:48:45 -0600

    Finish three tests for API

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -4,11 +4,23 @@ const app = require('../app');
 
 const api = supertest(app);
 
-test('notes are returned as json', async () => {
-    await api
-        .get('/api/notes')
-        .expect(200)
-        .expect('Content-Type', /application\/json/);
+describe('API', () => {
+    test('notes are returned as json', async () => {
+        await api
+            .get('/api/notes')
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+    })
+
+    test('there are five notes returned', async() => {
+        const res = await api.get('/api/notes')
+        expect(res.body.length).toBe(5);
+    })
+
+    test('the first note is about Express', async () => {
+        const res = await api.get('/api/notes');
+        expect(res.body[0].content).toBe('Express is a great framework')
+    })
 })
 
 afterAll(() => {
