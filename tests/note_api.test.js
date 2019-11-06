14bd08df31c3489afeba1d7295ac5a7d40feb27b
Peter Eck <jpeckiii@gmail.com>
2019-11-06 12:13:19 -0600

    Refactor beforeEach to use Promise.all

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,13 +8,9 @@ const api = supertest(app);
 
 beforeEach(async () => {
     await Note.deleteMany({});
-    console.log('cleared')
-    helper.initialNotes.forEach(async (note) => {
-        let noteObject = new Note(note)
-        await noteObject.save();
-        console.log('saved');
-    })
-    console.log('done');
+    const noteObjects = helper.initialNotes.map(note => new Note(note))
+    const promiseArray = noteObjects.map(note => note.save());
+    await Promise.all(promiseArray);
 })
 
 describe('API', () => {
