f1d6c778a1e56063e98dcdbc8868f9a2b5d67156
Peter Eck <jpeckiii@gmail.com>
2019-11-06 12:09:42 -0600

    Refactor beforeEach function

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,10 +8,13 @@ const api = supertest(app);
 
 beforeEach(async () => {
     await Note.deleteMany({});
-    let noteObject = new Note(helper.initialNotes[0])
-    await noteObject.save();
-    noteObject = new Note(helper.initialNotes[1])
-    await noteObject.save();
+    console.log('cleared')
+    helper.initialNotes.forEach(async (note) => {
+        let noteObject = new Note(note)
+        await noteObject.save();
+        console.log('saved');
+    })
+    console.log('done');
 })
 
 describe('API', () => {
@@ -61,6 +64,34 @@ describe('API', () => {
 
         const notesAtEnd = await helper.notesInDb();
         expect(notesAtEnd.length).toBe(helper.initialNotes.length)
+    })
+
+    test('a specific note can be viewed', async () => {
+        const notesAtStart = await helper.notesInDb();
+        const noteToView = notesAtStart[0];
+
+        const resultNote = 
+            await api
+                .get(`/api/notes/${noteToView.id}`)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+        expect(resultNote.body.content).toEqual(noteToView.content);
+    })
+
+    test('a note can be deleted', async () => {
+        const notesAtStart = await helper.notesInDb();
+        const noteToDelete = notesAtStart[0];
+
+        await api
+            .delete(`/api/notes/${noteToDelete.id}`)
+            .expect(204)
+        
+        const notesAtEnd = await helper.notesInDb();
+
+        expect(notesAtEnd.length).toBe(helper.initialNotes.length - 1)
+
+        const contents = notesAtEnd.map(note => note.content);
+        expect(contents).not.toContain(noteToDelete.content);
     })
 })
 
