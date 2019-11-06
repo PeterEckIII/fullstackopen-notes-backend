b5d2aa7868570d270adde5eded3feb67d48f2f74
Peter Eck <jpeckiii@gmail.com>
2019-11-06 11:06:56 -0600

    Adjust test for api

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Note = require('../models/note');
 
 const api = supertest(app);
+
+const initialNotes = [
+    {
+        content: 'HTML is easy',
+        date: new Date(),
+        important: false,
+    },
+    {
+        content: 'Browser can execute only Javascript',
+        date: new Date(),
+        important: true,
+    },
+]
+
+beforeEach(async () => {
+    await Note.deleteMany({});
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save();
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save();
+})
 
 describe('API', () => {
     test('notes are returned as json', async () => {
@@ -12,14 +34,15 @@ describe('API', () => {
             .expect('Content-Type', /application\/json/);
     })
 
-    test('there are five notes returned', async() => {
+    test('all notes are returned', async() => {
         const res = await api.get('/api/notes')
-        expect(res.body.length).toBe(5);
+        expect(res.body.length).toBe(initialNotes.length);
     })
 
-    test('the first note is about Express', async () => {
+    test('a specific note is within the returned notes', async () => {
         const res = await api.get('/api/notes');
-        expect(res.body[0].content).toBe('Express is a great framework')
+        const contents = res.body.map(r => r.content);
+        expect(contents).toContain('Browser can execute only Javascript')
     })
 })
 
